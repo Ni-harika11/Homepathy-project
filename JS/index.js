9bd871aa767488470458d05523ebe2d4bec06194
niharika <malikniharika048@gamil.com>
2026-02-21 12:38:43 +0530

    Initial commit

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".contact-slide");
+    let current = 0;
+
+    function showSlide(index) {
+        slides.forEach(slide => slide.classList.remove("active"));
+        slides[index].classList.add("active");
+    }
+
+    setInterval(() => {
+        current = (current + 1) % slides.length;
+        showSlide(current);
+    }, 2000);
+});
